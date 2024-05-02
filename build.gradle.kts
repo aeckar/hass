6b737cc6693535f231854356442e20ca2bed57c5
ladle <eckardt.angel7@outlook.com>
2024-05-02 03:02:03 -0400

    Added Maven plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.23"
     id("org.jetbrains.dokka") version "1.9.20"
+    `maven-publish`
 }
 
 group = "com.github.eckar"
@@ -14,6 +15,18 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.ladle-gh"
+            artifactId = "kanary"
+            version = "1.0"
+
+            from(components["kotlin"])
+        }
+    }
 }
 
 tasks.test {
